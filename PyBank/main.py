aba8e74bd2de9483fb1ddc7b9723b78f56a67252
buckleypj84 <50090899+buckleypj84@users.noreply.github.com>
2019-06-21 23:18:38 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,7 +13,7 @@
 #   ----------------------------
 #   Total Months: 86
 #   Total: $38382578
-#   Average  Change: $-2315.12 -- verify this amt
+#   Average  Change: $-2315.12 
 #   Greatest Increase in Profits: Feb-2012 ($1926159)
 #   Greatest Decrease in Profits: Sep-2013 ($-2196167)  
 #------------------------------------------------------------------------------- 
@@ -22,8 +22,7 @@ import csv
 
 #get the current working directory
 currentDir = os.getcwd()
-#print(currentDir)
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('..','..', 'Homework', 'budget_data.csv')
 
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -39,31 +38,33 @@ with open(csvpath, newline="") as csvfile:
 
     min_change = 0
     max_change = 0
-    x = 0
+   
+    prev_rec = 0
+    curr_rec = 0
 
     profit_loss_change = []
     month_yr = []
     budget = []
-    rowcnt = 0
- 
+
     #-----------------------------------------------------------------------------
     # Read through each row of data budget and month_yr lists
-    # cast qty in budget as float to prevent errors in calculations
+    #  
     #-----------------------------------------------------------------------------
     for row in csvreader:
         budget.append(float(row[1]))
         month_yr.append(row[0])
+        total_months += 1
 
-    #-----------------------------------------------------------------------------
-    # Read through each row of data 
-    #   begin reading at row 2 == subtract the qty in row 1 from qty in row 2
-    #   len(month_yr) == returns nbr of items in the list
-    #   load list with profit loss changes
-    #-----------------------------------------------------------------------------
-    for rowcnt in range(1,len(budget)):                          
-        total_months = len(month_yr)
-        pf_loss_change = float(budget[rowcnt]) - float(budget[rowcnt-1] )
+        curr_rec = int(row[1])
+        
+        if total_months == 1:
+            prev_rec = curr_rec
+
+        pf_loss_change = curr_rec - prev_rec
+
         profit_loss_change.append(pf_loss_change)
+
+        prev_rec = int(row[1])
 
     #----------------------------------------------------------------------------
     # Summarize and format budget data as currency with 2 decimals
@@ -84,21 +85,21 @@ with open(csvpath, newline="") as csvfile:
     # Retrieve the maximum/minimum profit lost change and the corresponding month and yr
     # on the month_yr list
     #----------------------------------------------------------------------------
-    max_change_date = str(month_yr[profit_loss_change.index(max(profit_loss_change))])
 
-    min_change_date = [profit_loss_change.index(min(profit_loss_change))]
+    max_change_date = str(month_yr[profit_loss_change.index(max(profit_loss_change))])
 
     min_change_date = str(month_yr[profit_loss_change.index(min(profit_loss_change))])
 
     #----------------------------------------------------------------------------
     # Calculate and format average budget change
     #----------------------------------------------------------------------------
-    p_average_change = sum(profit_loss_change)/total_months 
-    average_change = sum(profit_loss_change)/total_months 
+    p_average_change = sum(profit_loss_change)/(total_months - 1) 
+    average_change = sum(profit_loss_change)/(total_months - 1)
+    #average_change = sum(profit_loss_change)/(total_months - 1)
     average_change = ('$'+ format(average_change,',.2f'))
 
     #----------------------------------------------------------------------------
-    # Display data on the terminal and write data to a file
+    # Display data on the terminal and write data to a file  
     #----------------------------------------------------------------------------
     print("Financial Analysis")
     print("----------------------------------------")
@@ -111,6 +112,6 @@ with open(csvpath, newline="") as csvfile:
     budget_file.write("----------------------------------------" + "\n")
     budget_file.write(f"Total Months: {total_months}" + "\n")
     budget_file.write(f"Total Profit Loss: $ {p_total_profit_loss}" + " \n")
-    budget_file.write(f"Average Revenue Change: ${p_average_change}" + "\n")
+    budget_file.write(f"Average Revenue Change: {average_change}" + "\n")
     budget_file.write(f"Greatest Increase in Revenue: {max_change_date} (${p_max_change})" + "\n")
     budget_file.write(f"Greatest Decrease in Revenue: {min_change_date} (${p_min_change})" + "\n") 
